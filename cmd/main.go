8d68eaf2307ec62eaa3a484bdc40a8d4ea550890
N3v1 <n3v1010@gmail.com>
2023-11-19 20:45:36 +0100

    update .gitignore

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
-/*
+
 import (
 	"github.com/BlockSyncHQ/assets/internal/manager"
 )
+
+/*
 
 func main() {
 	manager.InitCommands()
--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -1,2 +1,5 @@
 package manager
 
+import (
+	"os"
+)
